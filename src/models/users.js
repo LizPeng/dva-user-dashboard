c442380c226e1ac00ade8ad743316d9dd9532e93
李志鹏 <lizhipeng@lizhipengdeMac-mini.local>
2017-11-28 23:40:49 +0800

    step 5 not done

---
+++ src/models/users.js
@@ -0,0 +1,18 @@
+import * as usersService from '../services/users';
+
+export default {
+  namespace: 'users',
+  state: {
+    list: [],
+    total: null,
+  },
+  reducers: {
+    save(state, { payload: { data: list, total } }) {
+      return { ...state, list, total }
+    },
+  },
+  effects: {
+    
+  },
+  subscriptions: {},
+};
