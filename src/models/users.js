76665ec2b08a12838379b5dab875871e33d72cd4
李志鹏 <lizhipeng@lizhipengdeMac-mini.local>
2017-12-04 22:53:41 +0800

    setp 6 done

--- src/models/users.js
+++ src/models/users.js
@@ -8,11 +8,22 @@ export default {
   },
   reducers: {
     save(state, { payload: { data: list, total } }) {
-      return { ...state, list, total }
+      return { ...state, list, total };
     },
   },
   effects: {
-    
+    *fetch({ payload: { page } }, { call, put }) {
+      const { data, headers } = yield call(usersService.fetch, { page });
+      yield put({ type: 'save', payload: { data, total: headers['x-total-count'] } });
+    },
   },
-  subscriptions: {},
+  subscriptions: {
+    setup({ dispatch, history }) {
+      return history.listen(({ pathname, query }) => {
+        if (pathname === '/users') {
+          dispatch({ type: 'fetch', payload: query });
+        }
+      });
+    },
+  },
 };
